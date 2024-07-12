d8492cdfd570ea27c88912929c02448266dc3853
UthayaRaj77 <uthayaraju77@gmail.com>
2024-07-12 11:02:54 +0530

    Task

---
+++ src/test/java/org/Git1/GitProject.java
@@ -0,0 +1,18 @@
+package org.Git1;
+
+public class GitProject {
+
+	private void Uthaya() {
+		System.out.println(1);
+	}
+
+	private void Raj() {
+			System.out.println(2);
+	}
+
+	private void Mani() {
+		System.out.println(3);
+
+	}
+
+}
